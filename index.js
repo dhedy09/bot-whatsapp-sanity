76d42a5dd490f7ab6709e1efd5ed36dbff4b8fee
dhedy09 <dedyjayakatwang@gmail.com>
2025-08-14 23:19:09 +0800

    auth 4

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ const clientSanity = createClient({
 });
 
 const client = new Client({
-    authStrategy: new LocalAuth()
+    authStrategy: new LocalAuth(),
     puppeteer: {
         headless: true,
         args: [
