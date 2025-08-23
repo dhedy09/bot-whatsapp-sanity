3cfbff9bfa46ac1b5947cdde951a5de76b5b1d47
dhedy09 <dedyjayakatwang@gmail.com>
2025-08-23 08:06:10 +0800

    PINDAH KE REPLIT

--- index.js
+++ index.js
@@ -9,7 +9,7 @@
 // BAGIAN 1: INISIALISASI & KONFIGURASI AWAL
 // =================================================================
 
-require('dotenv').config();
+// require('dotenv').config();==
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const express = require('express');
 const { Client, LocalAuth, MessageMedia } = require('whatsapp-web.js'); // MessageMedia ditambahkan
@@ -173,7 +173,8 @@ const clientSanity = createClient({
 });
 
 const client = new Client({
-    authStrategy: new LocalAuth({ dataPath: '/data/session' }),
+    // authStrategy: new LocalAuth({ dataPath: '/data/session' }),===
+    authStrategy: new LocalAuth(),
     puppeteer: {
         headless: true,
         args: [
